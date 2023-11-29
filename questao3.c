d91266d6e7fbc5761518ffc247b1063f5097dcff
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2023-11-29 13:56:54 -0300

    Update questao3.c

--- questao3.c
+++ questao3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+  int N_ant, N_prox, N, N_atual;
+N_ant = N_atual = 1;
+for (int i = 1; i<= N; i++)
+{
+N_prox = N_ant + N_atual;
+N_ant = N_atual;
+N_atual = N_prox;
+}
+printf("Fim do laco!\n");
+return 0;
+//correção do 0 e do ; no return e c 
+// ; faltando nos parentezes
+// valor atribuido a variavel em linha errada
